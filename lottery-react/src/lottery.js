a20e6a75ec84ce88cf5d1b64afecf85284a07dd4
Jose Carvajal <jose.carvajal@piksel.com>
2018-10-22 21:26:03 +0200

    Add lottery react app

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+// contract address
+const address = '0xF9b1a29BCc53517247cc35790b130f90C44431cF';
+// contract interface: abi
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
